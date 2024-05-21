1a67c3a16fb4068bb75ef33445ec82355e6f978a
Salman-sajib <sfsajib@gmail.com>
2024-05-21 14:23:32 +0600

    created the pages and components and define the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
+import DetailsPage from './pages/DetailsPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1 className=' text-5xl '>Hello World</h1>
+      <div className=' min-h-screen p-6 bg-white text-gray-600 text-lg '>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+          <Route path='/recipe-item/:id' element={<DetailsPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
